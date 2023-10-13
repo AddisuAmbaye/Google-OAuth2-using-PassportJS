77e912d2dd4d3ddcbd2d8ebb19107bb572fab16f
addis <addeson3@gmail.com>
2023-10-13 11:55:19 -0700

    Wrap up project: Google-OAuth2-using-PassportJS

--- auth.js
+++ auth.js
@@ -1,15 +1,21 @@
-const passport = require("passport");
+const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
+require('dotenv').config();
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_SECRET_ID,
-    callbackURL: "http://localhost:5000/google/callback",
-    passReqToCallback   : true
-  },
-  function(request, accessToken, refreshToken, profile, done) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return done(err, user);
-    });
-  }
-));
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_SECRET_ID,
+  callbackURL: "http://localhost:5000/auth/google/callback",
+  passReqToCallback: true,
+},
+function(request, accessToken, refreshToken, profile, done) {
+  return done(null, profile);
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
